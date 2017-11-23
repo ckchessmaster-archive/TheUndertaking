97460087b63f754a6189f4a6bb2b30b5e57f650f
Katie Grubbs <katie.grubbs@gmail.com>
2017-11-22 21:33:18 -0500

    added navigation bar to index.php

--- public_html/index.php
+++ public_html/index.php
@@ -1,7 +1,56 @@
 <!DOCTYPE html>
-<head>
-</head>
-<body>
-  Test page please ignore
-  Just testing -KG
-</body>
+<html lang="eng">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>The Undertaking</title>
+        <!-- CSS -->
+        <link rel="stylesheet" type="text/css" href="../resources/bootstrap-3.3.7-dist/css/bootstrap.min.css">
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+    </head>
+    <body>
+        <nav class="navbar navbar-default navbar-fixed-top">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="#">The Undertaking</a>
+                </div>
+                <div id="navbar" class="navbar-collapse collapse">
+                    <ul class="nav navbar-nav">
+                        <!-- Search bar here -->
+                    </ul>
+                    <ul class="nav navbar-nav navbar-right">
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Browse Games <span class="caret"></span></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="#">View All</a></li>
+                                <li role="separator" class="divider"></li>
+                                <li class="dropdown-header">By Genre</li>
+                                <li><a href="#">Action</a></li>
+                                <li><a href="#">Adventure</a></li>
+                                <li><a href="#">Casual</a></li>
+                                <li><a href="#">Indie</a></li>
+                                <li><a href="#">MMO</a></li>
+                                <li><a href="#">Racing</a></li>
+                                <li><a href="#">RPG</a></li>
+                                <li><a href="#">Simulation</a></li>
+                                <li><a href="#">Sports</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="#">New Releases</a></li>
+                        <li><a href="#">Log In</a></li>
+                    </ul>
+                </div><!--/.nav-collapse -->
+            </div>
+        </nav>
+        <!-- JavaScript -->
+        <script src="../resources/lib/jquery-3.2.1.min.js"></script>
+        <script src="../resources/bootstrap-3.3.7-dist/js/bootstrap.min.js"></script>
+    </body>
+</html>
