8fede2693b4057fda6e466f7ecd54df4d9be89c0
Katie Grubbs <katie.grubbs@gmail.com>
2017-12-03 01:21:31 -0500

    updated index.php structure

--- public_html/index.php
+++ public_html/index.php
@@ -22,14 +22,7 @@
                     <div class="page-header">
                         <h3>Featured Games</h3>
                     </div>
-                </div>
-                <div class="col-md-3"></div>
-            </div>
-            <!-- Carousel -->
-            <div class="row">
-                <div class="col-md-3"></div>
-                <div class="col-md-6">
-                    <button type="button" class="slick-prev">Previous</button>
+                    <!-- Carousel -->
                     <div class="carousel carousel-container">
                         <div class="carousel-content">
                             <div class="carousel-slide"><img src="media/featured1.jpg"></div>
@@ -37,24 +30,11 @@
                             <div class="carousel-slide"><img src="media/featured3.jpg"></div>
                         </div>
                     </div>
-                    <button type="button" class="slick-next"></button>
-                </div>
-                <div class="col-md-3"></div>
-            </div>
-            <!-- Website description header -->
-            <div class="row">
-                <div class="col-md-3"></div>
-                <div class="col-md-6">
+                    <!-- Website description header -->
                     <div class="page-header">
                         <h3>What is the Undertaking?</h3>
                     </div>
-                </div>
-                <div class="col-md-3"></div>
-            </div>
-            <!-- Website description -->
-            <div class="row">
-                <div class="col-md-3"></div>
-                <div class="col-md-6">
+                    <!-- Website description -->
                     <p>The Undertaking is Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                 </div>
                 <div class="col-md-3"></div>
