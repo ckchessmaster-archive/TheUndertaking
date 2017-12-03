02ca38aaf7398071482f81c225f3bdc58fccaf57
Katie Grubbs <katie.grubbs@gmail.com>
2017-12-03 02:13:13 -0500

    updated new-releases.php structure

--- public_html/new-releases.php
+++ public_html/new-releases.php
@@ -10,17 +10,11 @@
             <!-- "New Releases" header -->
             <div class="row">
                 <div class="col-md-3"></div>
-                <div class="col-md-6">
+                <div class="col-md-6 page">
                     <div class="page-header">
                         <h3>New Releases</h3>
                     </div>
-                </div>
-                <div class="col-md-3"></div>
-            </div>
-            <!-- Breadcrumbs -->
-            <div class="row">
-                <div class="col-md-3"></div>
-                <div class="col-md-6">
+                    <!-- Breadcrumbs -->
                     <ol class="breadcrumb">
                         <li><a href='index.php'>Home</a></li>
                         <li><a href='browse.php'>Browse Games</a></li>
