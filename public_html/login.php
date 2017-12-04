372a7ee65103b1fe3ba286c2306e23167d0b45c1
ckchessmaster <Genesis1:1>
2017-12-04 13:59:24 -0500

    finished basic structure for login page

--- public_html/login.php
+++ public_html/login.php
@@ -10,15 +10,17 @@ if(!isset($_GET["func"])) {
 // logout
 if($_GET["func"] == "logout") {
   session_destroy();
-  header('Location: login.php?function=login');
+  header('Location: login.php?func=login');
   exit();
-} else {
-  $_SESSION["loggedIn"] = true;
-  header('Location: index.php');
-  exit();
-}
-?>
-
+} else if($_GET["func"] == "validate") {
+  if(validateLogin()) {
+    header('Location: index.php');
+    exit();
+  } else {
+    header('Location: login.php?func=login&validate=failed');
+    exit();
+  }
+} ?>
 <!DOCTYPE html>
 <html lang="eng">
     <head>
@@ -28,18 +30,34 @@ if($_GET["func"] == "logout") {
         <?php include("shared/nav.php"); ?>
         <!-- Main content -->
         <div class="container-fluid">
-            <div class="row">
-                <div class="col-md-3"></div>
-                <div class="col-md-6 page">
-                    <!-- Search header -->
-                    <div class="page-header">
-                    </div>
-                    <!-- Results -->
-                    <div class="results results-container container-fluid">
-                    </div>
+          <div class="row">
+            <div class= "col-md-3 page">
+              <form action="login.php">
+                <input type="hidden" name="func" id="func" value="validate"/>
+                <div class="form-group">
+                  <h1>Login</h1>
+                  <div>Don't have an account?<a href="#">Sign up here!</a></div>
+                  <?php if(isset($_GET["validate"]) && $_GET["validate"] == "failed") { ?>
+                    <div class="text-danger"> Invalid Username or Password!</div> <?php
+                  } ?>
+                  <label for="inputUsername">Username</label>
+                  <input type="text" class="form-control" name="username" id="inputUsername" placeholder="Username" />
                 </div>
-                <div class="col-md-3"></div>
+                <div class="form-group">
+                  <label for="inputPassword">Username</label>
+                  <input type="password" class="form-control" name="password" id="inputPassword" placeholder="Password" />
+                </div>
+                <button type="submit" class="btn btn-success">Submit</button>
+                <div><a href="#">Forgot password?</a></div>
+              </form>
             </div>
+          </div>
         </div>
     </body>
 </html>
+
+<?php
+function validateLogin() {
+
+  return false;
+} ?>
