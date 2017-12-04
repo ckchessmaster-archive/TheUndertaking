87ec8e6e809b493feacfce5f247217db81e59da1
ckchessmaster <Genesis1:1>
2017-12-04 17:24:11 -0500

    fixed issue with mysql where PDO wouldn't display utf8 characters

--- resources/dbManager.php
+++ resources/dbManager.php
@@ -4,8 +4,9 @@ function getConnection() {
   $username = "group2";
   $password = "dxgBDmvja45!";
   $dbname = "group2";
+  $options = array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',);
 
-  $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+  $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password, $options);
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   return $conn;
